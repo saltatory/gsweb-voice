e275e21606351fd7609317915aa660a8b75d2d24
root <root@ip-10-127-155-242.ec2.internal>
2011-10-04 19:28:06 -0400

    Getting required modules to contact twilio

---
+++ chat.js
@@ -0,0 +1,7 @@
+var twilio = require('twilio');
+var TwilioCapability = require('TwilioCapability');
+var jwt = require('jwt');
+
+var authSID = "ACafeeac727f8a41f6803036f5c66c0227";
+var authToken = "0fc218a05239b184fc2462988ebeefdb";
+
