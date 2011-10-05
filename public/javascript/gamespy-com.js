f2acac723cae39aa7b5d32ff64e98015278d5441
Sean Lee <selee@ign.com>
2011-10-05 14:52:41 -0700

    Adding gamespy com client files

---
+++ public/javascript/gamespy-com.js
@@ -0,0 +1,149 @@
+var keys = {};
+var widgetShown = false;
+var widgetTimeout = false;
+var firstTime = true;
+function loadCSSFile(filename)
+{
+	var fileref=document.createElement("link");
+	fileref.setAttribute("rel", "stylesheet");
+	fileref.setAttribute("type", "text/css");
+	fileref.setAttribute("href", filename);
+	if (typeof fileref!="undefined")
+		document.getElementsByTagName("head")[0].appendChild(fileref);
+}
+
+$(function()
+{
+	$.fn.loadHtml = function(){
+		$.get('/html/gamespy-com.html', function(data)
+		{
+			$('body').append(data);
+		});
+	}
+});
+
+$(document).ready(function() {
+
+	
+	//load the CSS
+	loadCSSFile('/stylesheets/gamespy-com.css');
+	//load the HTML
+	$(document).loadHtml();
+
+	//logic to show/hide the widget when pressing Ctrl+V
+	$(document).keydown(function(){
+		keys[event.which] = true;
+
+		if(keys[17] && keys[86])
+		{
+			if(!widgetTimeout)
+			{
+				if(widgetShown)
+				{
+					$("#voice-widget-body").slideUp();
+					widgetShown = false;
+				}
+				else
+				{
+					$("#voice-widget-body").slideDown();
+					widgetShown = true;
+
+					//HACK
+					if(firstTime)
+					{
+						//remove these when you get the server reporting
+						$('#voice-widget-users').addUser('Sean');
+						$('#voice-widget-users').addUser('AzHP');
+						$('#voice-widget-users').addUser('BlueBerryP13');
+						firstTime = false;
+					}
+				}
+				widgetTimeout = true;
+			}
+		}
+	});
+
+	$(document).keyup(function(){
+		keys[event.which] = false;
+		if(!keys[86])
+		{
+			widgetTimeout = false;
+		}
+	});
+			
+	//sort the user list
+	(function($) {
+		$.fn.reOrder = function() {
+
+			var array = new Array();
+
+			$(this).children().each(function()
+			{
+				array.push($(this).attr('id'));
+			});
+
+			array.sort();
+
+	  	return this.each(function() {
+
+				for(var i=0; i < array.length; i++)
+					array[i] = $('#' + array[i]);
+				$(this).empty();  
+				for(var i=0; i < array.length; i++)
+					$(this).append(array[i]);      
+			});    
+		}
+	})(jQuery);
+			
+	var userList = new Array();
+	var thisUser = 'Sean';
+
+	(function($) {
+
+		$.fn.addUser = function(userName) {
+
+			var array = new Array();
+
+			$(this).children().each(function()
+			{
+				array.push($(this).attr('id'));
+			});
+			var newUserDiv = '<div class="user-name" id="un-' +
+				userName + '"><div class="user-name-text">' + userName +
+				'</div></div>';
+			if(array.length == 0)
+			{
+				$(this).append(newUserDiv);
+			}
+			else
+			{
+				var lastDiv = $('#' + array[array.length - 1]);
+				$(newUserDiv).insertAfter(lastDiv);
+				$('#voice-widget-users').reOrder();
+			}
+			userList.push(userName);
+		}
+	})(jQuery);
+			
+	(function($) {
+
+		$.fn.removeUser = function(userName) {
+			var array = new Array();
+
+			$(this).children().each(function()
+			{
+				if($(this).attr('id') == 'un-' + userName)
+				{
+					$(this).slideUp();
+					$(this).remove();
+				}
+			});
+		}
+	})(jQuery);
+			
+
+	$('#talk-button').click(function()
+	{
+			$('#voice-widget-users').removeUser('Sean');
+	});
+});
